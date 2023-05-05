2e1ae45a26f2438635212cf501ea6cf812697683
calgor <ahomayoni684@gmail.com>
2023-05-05 11:00:59 +0330

    added AddAccount Procedure

---
+++ server/database/db.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Calgorr/Banking_System/server/model"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "calgor"
+	password = "ami1r3ali"
+	dbname   = "Bank"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+func connect() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AddUser(user *model.User) error {
+	connect()
+	defer db.Close()
+	_, err := db.Exec("Call insert_account($1,$2,$3,$4,$5,$6,$7,$8,$9)", user.Username, user.Password, user.Accountnumber, user.Firstname, user.Lastname, user.Nationalid, user.Dateofbirth, user.Type, user.Interestrate)
+	return err
+}
+
+func GetUser(user *model.User) bool {
+
+}
--- server/model/user.go
+++ server/model/user.go
@@ -8,8 +8,15 @@ import (
 )
 
 type User struct {
-	Username string `json:"username" form:"username"`
-	Password string `json:"password" form:"password"`
+	Username      string `json:"username" form:"username"`
+	Password      string `json:"password" form:"password"`
+	Firstname     string `json:"firstname" form:"firstname"`
+	Lastname      string `json:"lastname" form:"lastname"`
+	Accountnumber string `json:"accountnumber" form:"accountnumber"`
+	Nationalid    string `json:"nationalid" form:"nationalid"`
+	Dateofbirth   string `json:"dateofbirth" form:"dateofbirth"`
+	Type          string `json:"type" form:"type"`
+	Interestrate  string `json:"interestrate" form:"interestrate"`
 }
 
 func (user *User) Bind(c echo.Context) (*User, error) {
